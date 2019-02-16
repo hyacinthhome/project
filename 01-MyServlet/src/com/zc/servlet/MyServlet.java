a31907fcdec2eb387ed5877d917eb653820d4328
Gin <hyacinthhome@outlook.com>
2019-02-16 13:20:39 +0800

    hello

---
+++ 01-MyServlet/src/com/zc/servlet/MyServlet.java
@@ -0,0 +1,35 @@
+package com.zc.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet{
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String name=request.getParameter("username");
+		String psd=request.getParameter("psd");
+		String info=request.getParameter("infomation");
+		System.out.println(info);
+		if(name.equals("abc")&&psd.equals("123")) {
+			out.print(true);
+		}else {
+			out.print(false);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doGet(request, response);
+	}
+}
